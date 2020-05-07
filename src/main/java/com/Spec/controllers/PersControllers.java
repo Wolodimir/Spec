a9a7d05666ede8f439cf907d9785a43b15a1371f
Wolodimir <dvdnent666@gmail.com>
2020-05-07 22:57:39 +0400

    Vladik voin works

--- src/main/java/com/Spec/controllers/PersControllers.java
+++ src/main/java/com/Spec/controllers/PersControllers.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PersControllers {
@@ -14,5 +16,22 @@ public class PersControllers {
     @GetMapping("/pers-add")
     public String persAdd(Model model){
         return "options/pers-add";
+    }
+
+    @PostMapping("/pers-add")
+    public String persMade(@RequestParam String name, @RequestParam String clas, Model model){
+
+        String spec_name;
+        switch (clas){
+            case "1": spec_name = "Воин";
+                break;
+            case "2": spec_name = "Маг";
+                break;
+            default: spec_name = "Никто";
+                break;
+        }
+        model.addAttribute("name", name);
+        model.addAttribute("clasName", spec_name);
+        return "index";
     }
 }
